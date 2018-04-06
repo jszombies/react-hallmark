ad9935e38ede8991060690fc1c0edf36fa2add39
Źmicier Jaraševič <zmeecer@gmail.com>
2018-04-06 11:30:27 +0300

    Feature/initial (#1)
    * add initial configs and deps
    
    add demo page
    
    * update deps and config
    
    add sample of filtering
    
    add functionality to prepare data
    
    * enhance demo page
    
    resolve review codestyle comments
    
    * make empty message configurable

---
+++ index.jsx
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react';
+import ReactDOM from 'react-dom';
+import Items from '../src';
+import theme from './theme.css';
+import './index.html';
+import array from '../mocks/array.json';
+import tree from '../mocks/tree.json';
+
+class App extends PureComponent {
+  state = {
+    query: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: '#444' }}>React Highlight Tiny</h1>
+        <input
+          style={{
+            display: 'block',
+            lineHeight: '26px',
+            width: '300px',
+            outline: 'none',
+            fontSize: '16px',
+            margin: '10px auto',
+            padding: '0 7px',
+          }}
+          placeholder="Search"
+          onChange={e => this.setState({ query: e.target.value })}
+        />
+        <div style={{ display: 'flex' }}>
+          <div style={{ width: '50%', paddingRight: '10px' }}>
+            <Items
+              items={array}
+              query={this.state.query}
+              theme={theme}
+              nothingView="Nothing to show"
+            />
+          </div>
+          <div style={{ width: '50%' }}>
+            <Items
+              items={tree}
+              query={this.state.query}
+              theme={theme}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
