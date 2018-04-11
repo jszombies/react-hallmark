b59a7b0a2e2caebf610b1644e1ea476def948a94
Źmicier Jaraševič <zmeecer@gmail.com>
2018-04-11 17:29:23 +0300

    add enzyme + setup files
    add test for the Items component

---
+++ src/tests/Items.spec.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Items from '../Items.jsx';
+import array from '../../mocks/array.json';
+
+describe('Items component', () => {
+  let output;
+
+  it('renders correctly', () => {
+    output = shallow(
+      <div>
+        <Items
+          items={array}
+          query=""
+        />
+      </div>,
+    );
+    expect(output.children('Items').length).toBe(1);
+  });
+
+  it('renders nothing view', () => {
+    const text = 'test';
+    output = shallow(
+      <Items
+        items={array}
+        query=""
+        nothingView={text}
+      />,
+    );
+    expect(output.text()).toBe(text);
+  });
+
+  it('renders one item', () => {
+    const text = 'Archive';
+    output = shallow(
+      <Items
+        items={array}
+        query={text}
+        nothingView="nothing"
+      />,
+    );
+    expect(output.text()).toBe(text);
+  });
+
+  it('renders two items', () => {
+    const text = 'a';
+    output = shallow(
+      <Items
+        items={array}
+        query={text}
+        nothingView="nothing"
+      />,
+    );
+    expect(output.find('a').length).toBe(2);
+  });
+
+  it('highlights the search phrase', () => {
+    const text = 'Arc';
+    output = shallow(
+      <Items
+        items={array}
+        query={text}
+        nothingView="nothing"
+      />,
+    );
+    expect(output.find('b').text()).toBe(text);
+  });
+});
